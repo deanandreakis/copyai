d0bec95c78dcff9ab177a43556ff77f16c36be1d
Dean Andreakis <dean@deanware.com>
2022-06-03 14:59:09 -0700

    updates

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,4 +1,9 @@
+# contact class
 class ContactController < ApplicationController
   def home
+  end
+
+  def create
+    # parse the post reponse. User has posted a message
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  root "static_pages#home"
-  get "/faq", to: "static_pages#faq"
-  get "/home", to: "static_pages#home"
-  get "/tools", to: "tools#home"
-  get "/contact", to: "contact#home"
-  get "/pricing", to: "pricing#home"
-  get "/privacy", to: "static_pages#privacy"
-  get "/terms", to: "static_pages#terms"
+  root 'static_pages#home'
+  get '/faq', to: 'static_pages#faq'
+  get '/home', to: 'static_pages#home'
+  get '/tools', to: 'tools#home'
+  get '/contact', to: 'contact#home'
+  get '/pricing', to: 'pricing#home'
+  get '/privacy', to: 'static_pages#privacy'
+  get '/terms', to: 'static_pages#terms'
+  post '/contact', to: 'contact#create'
 end
